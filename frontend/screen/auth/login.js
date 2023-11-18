3ce541d5a637e1d1db78f16d2f6443f0d27e376d
rafaelrnzo <rl.lorenzo.256@gmail.com>
2023-11-18 16:11:39 +0700

    commit

---
+++ frontend/screen/auth/login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function LoginScreen () {
+    return (
+      <View className="flex-1">
+        
+        <Text className="text-2xl">s</Text>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default LoginScreen;
